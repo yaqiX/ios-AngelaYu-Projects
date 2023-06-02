aadc87d3a0a6a6bc4ca09fa69762dc51b1feb92c
Yaqi Xie <91613914+yaqiX@users.noreply.github.com>
2023-06-02 10:58:37 -0700

    Re add i am rich

---
+++ i-am-Rich/iamRich/iamRich/iamRich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iamRich
+//
+//  Created by Xieh on 2023/6/1.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
