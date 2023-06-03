11f9ae5bbf6436dbc55d1bafcf120e298540b3fc
Yaqi Xie <91613914+yaqiX@users.noreply.github.com>
2023-06-02 19:58:37 -0700

    image view change dicee

---
+++ Dicee-iOS13/Dicee-iOS13/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Dicee-iOS13
+//
+//  Created by Angela Yu on 11/06/2019.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        diceImageView1.image = UIImage(named: "DiceSix")
+        diceImageView1.alpha = 0.5
+        diceImageView2.image = UIImage(named: "DiceThree")
+
+        
+    }
+
+
+}
+
