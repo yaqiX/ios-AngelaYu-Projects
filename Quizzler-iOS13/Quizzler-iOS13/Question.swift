9a0d2cbbf19cf3a869ee50f4f07d57ebafc72752
Yaqi Xie <91613914+yaqiX@users.noreply.github.com>
2023-06-10 13:50:23 -0700

    Updated questions in Quizzler

---
+++ Quizzler-iOS13/Quizzler-iOS13/Question.swift
@@ -0,0 +1,14 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by Xieh on 2023/6/10.
+//  Copyright © 2023 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    let text: String
+    let answer: String
+}
--- Quizzler-iOS13/Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/Quizzler-iOS13/ViewController.swift
@@ -10,11 +10,51 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    
+    let quiz = [
+        Question(text:"The Earth is the largest planet in our solar system.", answer:"False"),
+        Question(text:  "The Great Wall of China is visible from space.", answer: "False"),
+        Question(text: "Dolphins are mammals.", answer: "True"),
+        Question(text: "The Eiffel Tower is located in London.", answer: "False"),
+        Question(text: "Water boils at 100 degrees Fahrenheit (212 degrees Celsius).", answer: "False")
+    ]
+    var questionNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateUI()
     }
-
-
+    
+    
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        // will be trigegred either of the button pressed
+        let userAnswer = sender.currentTitle //T or F
+        let actualQuestion = quiz[questionNumber]
+        let actualAnswer = actualQuestion.answer
+        
+        if userAnswer == actualAnswer{
+            print("Right!")
+        }else{
+            print("Wrong!")
+        }
+        
+        if questionNumber + 1 < quiz.count {
+            questionNumber += 1
+        }else {
+            questionNumber  = 0
+        }
+        
+        updateUI()
+    }
+    
+    func updateUI(){
+        questionLabel.text = quiz[questionNumber].text;]
+    }
+    
 }
 
