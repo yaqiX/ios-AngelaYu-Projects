85448d94e2c67f4c746f1320ca1e5d5f819b2ae7
Yaqi Xie <91613914+yaqiX@users.noreply.github.com>
2023-06-12 22:45:46 -0700

    Finish Multichoices Quizz

--- Quizzler-iOS13/Quizzler-iOS13/Controller/ViewController.swift
+++ Quizzler-iOS13/Quizzler-iOS13/Controller/ViewController.swift
@@ -12,8 +12,11 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var progressBar: UIProgressView!
-    @IBOutlet weak var trueButton: UIButton!
-    @IBOutlet weak var falseButton: UIButton!
+
+    @IBOutlet weak var choice1: UIButton!
+    @IBOutlet weak var choice2: UIButton!
+    @IBOutlet weak var choice3: UIButton!
+    
     @IBOutlet weak var scoreLabel: UILabel!
     
     var quizBrain = QuizBrain()
@@ -24,10 +27,9 @@ class ViewController: UIViewController {
         updateUI()
     }
     
-    
     @IBAction func answerButtonPressed(_ sender: UIButton) {
         // will be trigegred either of the button pressed
-        let userAnswer = sender.currentTitle! //T or F
+        let userAnswer = sender.currentTitle!
         let userGotItRight = quizBrain.checkAnswer(userAnswer)
         
         if userGotItRight {
@@ -43,13 +45,18 @@ class ViewController: UIViewController {
     
     @objc func updateUI(){
         questionLabel.text = quizBrain.getQuestionText()
-            // change the color back
+        
+        let answerChoices = quizBrain.getAnswers()
+        choice1.setTitle(answerChoices[0], for: .normal)
+        choice2.setTitle(answerChoices[1], for: .normal)
+        choice3.setTitle(answerChoices[2], for: .normal)
+        
         progressBar.progress = quizBrain.getProgress()
         scoreLabel.text = "Score: \(quizBrain.getScore())"
         
-        trueButton.backgroundColor = UIColor.clear
-        falseButton.backgroundColor = UIColor.clear
-      
+        choice1.backgroundColor = UIColor.clear
+        choice2.backgroundColor = UIColor.clear
+        choice3.backgroundColor = UIColor.clear
             }
     
 }
--- Quizzler-iOS13/Quizzler-iOS13/Model/Question.swift
+++ Quizzler-iOS13/Quizzler-iOS13/Model/Question.swift
@@ -10,10 +10,12 @@ import Foundation
 
 struct Question {
     let text: String
-    let answer: String
+    let answers: [String] //Multiple choice questions have multiple answers
+    let rightAnswer: String
     
-    init(q:String, a:String){
+    init(q:String, a:[String], correctAnswer: String){
         text = q
-        answer = a
+        answers = a
+        rightAnswer = correctAnswer
     }
 }
