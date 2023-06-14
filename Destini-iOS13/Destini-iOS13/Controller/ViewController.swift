bf4ae079957621a32fef14d8fed5e067f6d3a4af
Yaqi Xie <91613914+yaqiX@users.noreply.github.com>
2023-06-13 23:55:26 -0700

    update stories in Destini

--- Destini-iOS13/Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Destini-iOS13/Controller/ViewController.swift
@@ -18,13 +18,27 @@ class ViewController: UIViewController {
     let choice1 = "Take a left..."
     let choice2 = "Take a right..."
     
+    let stories = [Story(title: "You see a fork in the road.", choice1: "Take a left...", choice2: "Take a right..."),
+                   Story(title: "You see a Tiger", choice1: "Shout for HELP", choice2: "Play Dead"),
+                   Story(title: "You found a treasure chest.", choice1: "Open it", choice2: "Check for traps"),
+    ]
+       
     override func viewDidLoad() {
         super.viewDidLoad()
-        storyLabel.text = story0
-        choice1Button.setTitle(choice1, for: .normal)
-        choice2Button.setTitle(choice2, for: .normal)
+        
+        
     }
-
+    
+    @IBAction func choiceMade(_ sender: UIButton) -> String! {
+        let choiceText = sender.currentTitle!
+        
+        if choiceText == "Take a left"{
+            return stories[1].story
+        } else{
+            return stories[2].story
+        }
+        
+    }
 
 }
 
--- Destini-iOS13/Destini-iOS13/Model/Story.swift
+++ Destini-iOS13/Destini-iOS13/Model/Story.swift
@@ -7,3 +7,17 @@
 //
 
 import Foundation
+struct Story {
+    let story: String
+    let choice1: String
+    let choice2: String
+    
+    init(title: String, choice1: String, choice2: String){
+        story = title
+        self.choice1 = choice1
+        self.choice2 = choice2
+    }
+    
+    
+}
+
