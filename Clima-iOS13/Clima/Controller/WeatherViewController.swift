76b41af84657ae16facce9056ec2751f665acd2e
Yaqi Xie <91613914+yaqiX@users.noreply.github.com>
2023-06-27 23:29:56 -0700

    add Clima app for weather tracking

---
+++ Clima-iOS13/Clima/Controller/WeatherViewController.swift
@@ -0,0 +1,59 @@
+//
+//  ViewController.swift
+//  Clima
+//
+//  Created by Angela Yu on 01/09/2019.
+//  Copyright © 2019 App Brewery. All rights reserved.
+//
+// UITextFieldDelegate
+import UIKit
+
+class WeatherViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        searchTextField.delegate = self // will respond to keyboard
+    }
+
+    @IBAction func searchPressed(_ sender: UIButton) {
+        searchTextField.endEditing(true)
+        print(searchTextField.text!)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchTextField.endEditing(true)
+        print(searchTextField.text!)
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        /* we are not triggering the function
+        the textfield class is calling the methods
+        you can get multiple textfields or triggering the same methods
+         if ibfunction link with many methods,
+            it depends on who detected the event
+         */
+        
+        if textField.text != ""{
+            return true
+        } else{
+            textField.placeholder = "Enter the Location Here"
+            return false
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        //after done editing
+        searchTextField.text = ""
+        // use the searchTextField.text to get the weather for city
+        
+    }
+    
+}
+
