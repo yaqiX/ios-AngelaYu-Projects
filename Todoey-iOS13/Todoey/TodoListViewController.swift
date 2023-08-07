e426184fd11cdaccb2d6a9ca63ba8d8f1e5a83fa
Yaqi Xie <91613914+yaqiX@users.noreply.github.com>
2023-08-06 21:41:20 -0700

    add nav todoList Project

---
+++ Todoey-iOS13/Todoey/TodoListViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Philipp Muellauer on 02/12/2019.
+//  Copyright © 2019 App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+
+    let itemArray = ["Find Mike","Buy Eggs","Drink Milk"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+// MARK: - Tableview datasource
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        let item = itemArray[indexPath.row]
+        
+        cell.textLabel?.text = item
+        
+        //cell.accessoryType = item.done ? .checkmark : .none
+        
+        return cell
+    }
+}
+
